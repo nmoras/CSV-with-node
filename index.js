9d1973f3023fd5c93adbfde98d0b0cc69bee93b2
Norma <normamoras@gmail.com>
2021-08-31 15:20:17 -0400

    filter array

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var fs = require('fs');
 var parse = require('csv-parse');
-var parser = parse({columns: true}, function (err, records) {
-	console.log(records);
+let keywordList;
+var parser = parse(function (err, records) {
+	//console.log(records[1][0]);
+    keywordList = records.map( x => x[0])
+    console.log(keywordList)
 });
+
 
 fs.createReadStream(__dirname+'/vacation_home_rentals.csv').pipe(parser);
