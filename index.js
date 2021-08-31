de2f7a30ed264c990cbf6307c25630078bff2302
Norma <normamoras@gmail.com>
2021-08-31 15:38:17 -0400

    map function

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var fs = require('fs');
 var parse = require('csv-parse');
+let homeKeywords;
 let keywordList;
 var parser = parse(function (err, records) {
 	//console.log(records[1][0]);
-    keywordList = records.map( x => x[0])
-    console.log(keywordList)
+    //keywordList = records.map( x => x[1])
+    //console.log(keywordList)
+
+    homeKeywords = records.map( x => {
+        if (x[1] >= 200){
+            return x;
+        } 
+    })
+    console.log(homeKeywords)
 });
 
 
